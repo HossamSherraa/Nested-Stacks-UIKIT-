6ce730b4e2ca378005862abf5a85f4e82a9ae784
HossamSherraa <54014933+HossamSherraa@users.noreply.github.com>
2021-02-15 18:13:48 +0200

    init

---
+++ Stretchy Header/CustomeView.swift
@@ -0,0 +1,12 @@
+//
+//  CustomeView.swift
+//  Stretchy Header
+//
+//  Created by Hossam on 11/14/19.
+//  Copyright © 2019 Hossam. All rights reserved.
+//
+
+import UIKit
+class CustomeView : UIView {
+    
+}
